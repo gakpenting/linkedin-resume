426bffaef349448957cf6bfb41d89888c8c2c889
spiritbro1 <rinoakbr@gmail.com>
2020-11-24 16:47:32 +0700

    upload ke dropbox kurang bahasa indonesia

--- dropbox.js
+++ dropbox.js
@@ -2,18 +2,17 @@ const dropbox_1 = require("dropbox"); // eslint-disable-line no-unused-vars
 const fs = require("fs");
 const path = require("path");
 
-    const dbx = new dropbox_1.Dropbox({ accessToken: process.env.DROPBOX_KEY });
-    fs.readFile(path.join(__dirname, '/febriano-resume.pdf'), 'utf8', async (err, contents) => {
-        if (err) {
-            console.log('Error: ', err);
-        }
-        await dbx.filesDeleteV2({path:"/febriano-resume.pdf"})
-        // This uploads basic.js to the root of your dropbox
-        dbx.filesUpload({ path: '/febriano-resume.pdf', contents })
-            .then((response) => {
-            console.log(response);
-        })
-            .catch((uploadErr) => {
-            console.log(uploadErr);
-        });
-    });
+    const dropboxV2Api = require('dropbox-v2-api');
+    const dropbox = dropboxV2Api.authenticate({
+      token: process.env.DROPBOX_KEY
+  });
+    dropbox({
+      resource: 'files/upload',
+      parameters: {
+          path: '/febriano-resume.pdf',
+          mode:"overwrite"
+      },
+      readStream: fs.createReadStream(path.join(__dirname, '/febriano-resume.pdf'))
+  }, (err, result, response) => {
+      console.log(response)
+  });
